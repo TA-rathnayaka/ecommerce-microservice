42aa99ec2e6921c61a3c1ddb8cfa1c09f7e6eb34
amamibhadya <wamamibhadya99@gmail.com>
2026-04-23 19:57:01 +0530

    update height

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -215,7 +215,7 @@ export { Login };
 /* ── Styles ── */
 const pageStyle = {
   position: "relative",
-  minHeight: "calc(100vh - 72px)",
+  minHeight: "100vh ",
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
